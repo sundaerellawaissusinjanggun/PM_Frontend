6133cc32ee1b9f6fa712789e3e09b31a8a1f6542
ZUITOPIA <korhwngml123@naver.com>
2024-09-20 14:58:13 +0900

    feat: 글로벌 스타일 적용 및 기존 css 파일 삭제

--- pigme/src/App.jsx
+++ pigme/src/App.jsx
@@ -1,9 +1,18 @@
-import Test from "./services/test";
+import styled from "@emotion/styled";
+import Login from "./pages/login";
+// import Test from "./services/test";
 
 export default function App() {
     return (
-        <>
-            <Test />
-        </>
+        <Style.Wrapper>
+            {/* <Test /> */}
+            <Login />
+        </Style.Wrapper>
     );
 }
+
+const Style = {
+    Wrapper: styled.div`
+        border: 1px solid red;
+    `,
+};
--- pigme/src/pages/Login/index.jsx
+++ pigme/src/pages/Login/index.jsx
@@ -1,3 +1,63 @@
+import styled from "@emotion/styled";
+
+import Pig from "/public/pig.svg";
+import LogoText from "/public/logo-text.svg";
+
 export default function Login() {
-    return <>소셜 로그인 페이지입니다</>;
+    return (
+        <>
+            <Style.Wrapper>
+                <Style.LogoWrapper>
+                    <Style.LogoImage>
+                        <img src={Pig} />
+                    </Style.LogoImage>
+                    <img src={LogoText} />
+                </Style.LogoWrapper>
+                <Style.Footer>
+                    <Style.LoginButton>로그인</Style.LoginButton>
+                    <Style.CopyRight>Copyright ⓒ 2024. 순대렐라와이쑤신장군. All rights reserved.</Style.CopyRight>
+                </Style.Footer>
+            </Style.Wrapper>
+        </>
+    );
 }
+
+const Style = {
+    Wrapper: styled.div`
+        width: 390px;
+        height: 844px;
+        background: url(public/background.svg);
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        flex-direction: column;
+        position: relative;
+    `,
+    LogoWrapper: styled.div`
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        flex-direction: column;
+        margin: 20px;
+    `,
+    LogoImage: styled.div`
+        padding: 20px;
+    `,
+    Footer: styled.div`
+        position: absolute;
+        bottom: 0;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        width: 100%;
+    `,
+    LoginButton: styled.div`
+        padding: 0 0 30px;
+    `,
+    CopyRight: styled.div`
+        color: #dadada;
+        font-size: 10px;
+        margin-top: auto;
+        padding: 0 0 15px;
+    `,
+};
