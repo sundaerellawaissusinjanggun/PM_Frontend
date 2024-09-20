b59a0c4ec8f680b388388289bd31fcbbcab60df6
ZUITOPIA <korhwngml123@naver.com>
2024-09-20 15:23:39 +0900

    feat: 기본 스타일 theme, ui 파일 설정

---
+++ pigme/src/styles/theme.js
@@ -0,0 +1,33 @@
+const color = {
+    black: "#1a1a1a",
+    white: "#fff",
+    grayDeep: "#979797",
+    grayLight: "#dbdbdb",
+    grayDate: "#888",
+    inputBackground: "#e3e3e35f",
+    sendMessageLogo: "#445ffe",
+    senderMessageBox: "#6245ff",
+    receiverMessageBox: "#e9e9e9",
+};
+
+const fontSize = {
+    small: "12px",
+    medium: "14px",
+    subtitle: "18px",
+    title: "32px",
+};
+
+const fontWeight = {
+    light: "300",
+    normal: "400",
+    medium: "500",
+    bold: "700",
+};
+
+const theme = {
+    color,
+    fontSize,
+    fontWeight,
+};
+
+export default theme;
