5497a1e3162fe176ed459835ef0536eabcfd8dec
ZUITOPIA <korhwngml123@naver.com>
2024-09-24 16:09:29 +0900

    feat: react-modal을 이용하여 기본 모달 형태 생성

--- pigme/src/pages/Home/index.jsx
+++ pigme/src/pages/Home/index.jsx
@@ -1,3 +1,22 @@
+import { useState } from "react";
+import BasicModal from "../../components/Modal/BasicModal";
+import { Block, Text } from "../../styles/UI";
+
 export default function Home() {
-    return <>메인 홈입니다.</>;
+    const [isModalOpen, setIsModalOpen] = useState(false);
+
+    const openModal = () => setIsModalOpen(true);
+
+    return (
+        <>
+            <button onClick={openModal}>모달 열기</button>
+            <BasicModal isOpen={isModalOpen} setIsOpen={setIsModalOpen} width="340px" height="292px">
+                <Block.FlexBox direction="column" alignItems="center">
+                    <Text.Body1>경고</Text.Body1>
+                    <Text.Body1>로그인이 필요한 기능입니다!</Text.Body1>
+                    <Text.Body1>로그인 페이지로 이동하기</Text.Body1>
+                </Block.FlexBox>
+            </BasicModal>
+        </>
+    );
 }
