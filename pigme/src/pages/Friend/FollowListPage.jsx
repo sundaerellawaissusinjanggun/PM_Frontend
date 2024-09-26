550e3970c7c148f464343685ce16c69c3f991f7f
kyoul10121 <kyoul10121@naver.com>
2024-09-27 03:51:51 +0900

    feat: 받은 메세지가 1개 이하일 때 경고문 띄우는 기법;

--- pigme/src/pages/Friend/FollowListPage.jsx
+++ pigme/src/pages/Friend/FollowListPage.jsx
@@ -42,6 +42,7 @@ export default function FollowListPage() {
           <Block.FlexBox padding="20px 0">
             <Text.Title>친구 요청 목록</Text.Title>
           </Block.FlexBox>
+
           {/* 친구 목록 개수 영역 */}
           <Block.FlexBox justifyContent="flex-end" margin="10px 0">
             <Text.Body1 weight="bold" color="black">
@@ -50,53 +51,65 @@ export default function FollowListPage() {
             <Text.Body1 weight="bold">의 친구 요청</Text.Body1>
           </Block.FlexBox>
 
-          {/* 친구 목록 영역 */}
-          {friends.map((friend) => (
-            <Block.FlexBox
-              key={friend.id}
-              padding="20px 0 "
-              borderBottom="2px solid #E7E7E7"
-            >
-              <Block.FlexBox gap="10px">
-                {/* 친구의 프로필 사진 */}
-                <Img.AngledIcon src={Pig} width="21px" />
-                <Block.FlexBox>
-                  {/* 친구의 닉네임 */}
-                  <Text.ModalText>{friend.name}</Text.ModalText>
-                </Block.FlexBox>
-              </Block.FlexBox>
-              {/* 수락/거절 버튼 */}
-              <Block.FlexBox>
-                {friend.isAccepted ? (
-                  <ButtonWrapper>
-                    <Text.ButtonText color="grayDeep">
-                      친구가 되었어요!
-                    </Text.ButtonText>
-                  </ButtonWrapper>
-                ) : (
-                  <ButtonWrapper>
-                    <Button.FriendBtn
-                      width="43px"
-                      height="24px"
-                      bgColor="white"
-                      onClick={() => handleDelete(friend.id)}
-                    >
-                      <Text.ButtonText color="grayDeep">거절</Text.ButtonText>
-                    </Button.FriendBtn>
-                    <Button.FriendBtn
-                      width="43px"
-                      height="24px"
-                      bgColor="pink"
-                      border="none"
-                      onClick={() => handleAccept(friend.id)}
-                    >
-                      <Text.ButtonText color="white">수락</Text.ButtonText>
-                    </Button.FriendBtn>
-                  </ButtonWrapper>
-                )}
-              </Block.FlexBox>
+          {friends.length === 0 ? (
+            <Block.FlexBox justifyContent="center" alignItem="center">
+              <Text.ModalText color="#E7E7E7">
+                아직 친구가 없네요! 먼저 친구 요청을 보내서 친구를 만들어보세요.
+              </Text.ModalText>
             </Block.FlexBox>
-          ))}
+          ) : (
+            <>
+              {/* 친구 목록 영역 */}
+              {friends.map((friend) => (
+                <Block.FlexBox
+                  key={friend.id}
+                  padding="20px 0 "
+                  borderBottom="2px solid #E7E7E7"
+                >
+                  <Block.FlexBox gap="10px">
+                    {/* 친구의 프로필 사진 */}
+                    <Img.AngledIcon src={Pig} width="21px" />
+                    <Block.FlexBox>
+                      {/* 친구의 닉네임 */}
+                      <Text.ModalText>{friend.name}</Text.ModalText>
+                    </Block.FlexBox>
+                  </Block.FlexBox>
+                  {/* 수락/거절 버튼 */}
+                  <Block.FlexBox>
+                    {friend.isAccepted ? (
+                      <ButtonWrapper>
+                        <Text.ButtonText color="grayDeep">
+                          친구가 되었어요!
+                        </Text.ButtonText>
+                      </ButtonWrapper>
+                    ) : (
+                      <ButtonWrapper>
+                        <Button.FriendBtn
+                          width="43px"
+                          height="24px"
+                          bgColor="white"
+                          onClick={() => handleDelete(friend.id)}
+                        >
+                          <Text.ButtonText color="grayDeep">
+                            거절
+                          </Text.ButtonText>
+                        </Button.FriendBtn>
+                        <Button.FriendBtn
+                          width="43px"
+                          height="24px"
+                          bgColor="pink"
+                          border="none"
+                          onClick={() => handleAccept(friend.id)}
+                        >
+                          <Text.ButtonText color="white">수락</Text.ButtonText>
+                        </Button.FriendBtn>
+                      </ButtonWrapper>
+                    )}
+                  </Block.FlexBox>
+                </Block.FlexBox>
+              ))}
+            </>
+          )}
         </Block.FlexBox>
       </Block.BackgroundWhiteBox>
     </>
