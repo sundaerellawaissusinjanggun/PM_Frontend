f59d2df0cf6654b3244e449e4f785241eef5907e
kyoul10121 <kyoul10121@naver.com>
2024-09-26 11:44:19 +0900

    feat: 삭제 성공 모달 추가

--- pigme/src/components/DeleteMessge.jsx
+++ pigme/src/components/DeleteMessge.jsx
@@ -1,13 +1,26 @@
 // 좋아요 개수, 좋아요 목록
 import styled from '@emotion/styled';
-import Heart from '/red-heart.svg';
-import { Text } from '../styles/UI';
-import { useState } from 'react';
+import WarningModal from '../components/Modal/WarningModal.jsx';
+import useModal from '../components/Hooks/useModal';
+import { useNavigate } from 'react-router-dom';
+import Smile from '/sunglasses-smile.svg';
 
-export default function Like() {
+export default function DeleteButton() {
+  // 경고 모달
+  const warningModal = useModal();
+  const navigate = useNavigate();
+  const handleGoToMainHome = () => navigate('/home');
   return (
     <>
-      <DeleteButton>삭제하기</DeleteButton>
+      <WarningModal
+        isOpen={warningModal.isOpen}
+        setIsOpen={warningModal.setIsOpen}
+        message="성공적으로 삭제되었어요!"
+        actionText="메인페이지로 이동하기"
+        onAction={handleGoToMainHome}
+        imageSrc={Smile}
+      />
+      <DeleteButton onClick={warningModal.openModal}>삭제하기</DeleteButton>
     </>
   );
 }
