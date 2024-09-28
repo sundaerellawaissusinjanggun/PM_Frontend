81397eeff3e0f02dcf27184a32fd28a923b79471
kyoul10121 <kyoul10121@naver.com>
2024-09-28 13:44:11 +0900

    feat: 글 삭제 확인 모달 추가 후 페이지 연결

--- pigme/src/components/DeleteMessge.jsx
+++ pigme/src/components/DeleteMessge.jsx
@@ -5,14 +5,27 @@ import WarningModal from '../components/Modal/WarningModal.jsx';
 import useModal from '../components/Hooks/useModal';
 import { useNavigate } from 'react-router-dom';
 import Smile from '/sunglasses-smile.svg';
-
+import CancleModal from '../components/Modal/CancleModal.jsx';
+import Caution from '/caution-icon.svg';
 export default function DeleteMessage() {
   // 경고 모달
   const warningModal = useModal();
+  const cancleModal = useModal();
   const navigate = useNavigate();
   const handleGoToMainHome = () => navigate('/home');
   return (
     <>
+      {/* 작성 중 삭제 확인 모달 */}
+      <CancleModal
+        isOpen={cancleModal.isOpen}
+        setIsOpen={cancleModal.setIsOpen}
+        imageSrc={Caution}
+        message={'삭제하면 다시 되돌릴 수 없어요!\n정말 삭제하시겠어요?'}
+        cancelText={'아니오'}
+        confirmText={'네 삭제할래요'}
+        onConfirm={warningModal.openModal}
+      />
+      {/* 삭제 성공 모달 */}
       <WarningModal
         isOpen={warningModal.isOpen}
         setIsOpen={warningModal.setIsOpen}
@@ -20,8 +33,9 @@ export default function DeleteMessage() {
         actionText="메인페이지로 이동하기"
         onAction={handleGoToMainHome}
         imageSrc={Smile}
+        style={{ height: '303px' }}
       />
-      <DeleteButton onClick={warningModal.openModal}>삭제하기</DeleteButton>
+      <DeleteButton onClick={cancleModal.openModal}>삭제하기</DeleteButton>
     </>
   );
 }
